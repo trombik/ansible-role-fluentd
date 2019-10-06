4e1c588d51f4441652708ff7b5e8b492f3a9e38c
Tomoyuki Sakurai <y@trombik.org>
2019-10-06 16:45:58 +0900

    update the example plugin

--- files/in_example.rb
+++ files/in_example.rb
@@ -1,20 +1,22 @@
-require "fluent/input"
+require "fluent/plugin/input"
 
 module Fluent
-  # An example fluentd plug-in
-  class Example < Input
-    Fluent::Plugin.register_input("example", self)
+  module Plugin
+    # An example fluentd plug-in
+    class Example < Input
+      Fluent::Plugin.register_input("example", self)
 
-    def configure(conf)
-      super
-    end
+      def configure(conf)
+        super
+      end
 
-    def start
-      super
-    end
+      def start
+        super
+      end
 
-    def shutdown
-      super
+      def shutdown
+        super
+      end
     end
   end
 end
