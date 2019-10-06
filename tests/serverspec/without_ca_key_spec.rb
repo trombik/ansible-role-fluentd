c2844f9c7901f11fef21f184e246925b34a12d1b
Tomoyuki Sakurai <y@trombik.org>
2019-10-06 18:48:26 +0900

    bugfix: QA

--- tests/serverspec/without_ca_key_spec.rb
+++ tests/serverspec/without_ca_key_spec.rb
@@ -1,32 +1,30 @@
-require 'spec_helper'
-require 'serverspec'
+require "spec_helper"
+require "serverspec"
 
-fluentd_package_name = 'td-agent'
-fluentd_service_name = 'td-agent'
-fluentd_config_dir   = '/etc/td-agent/conf.d'
-fluentd_config       = '/etc/td-agent/td-agent.conf'
-fluentd_user_name    = 'td-agent'
-fluentd_user_group   = 'td-agent'
-fluentd_gem_bin      = '/usr/sbin/td-agent-gem'
-fluentd_certs_dir    = '/etc/td-agent/certs'
-fluentd_buffer_dir   = '/var/spool/fluentd'
-fluentd_unix_pipe_dir= '/var/tmp/fluentd'
+fluentd_package_name = "td-agent"
+fluentd_service_name = "td-agent"
+fluentd_config_dir   = "/etc/td-agent/conf.d"
+fluentd_user_name    = "td-agent"
+fluentd_user_group   = "td-agent"
+fluentd_gem_bin      = "/usr/sbin/td-agent-gem"
+fluentd_certs_dir    = "/etc/td-agent/certs"
+fluentd_buffer_dir   = "/var/spool/fluentd"
+fluentd_unix_pipe_dir = "/var/tmp/fluentd"
 
 case os[:family]
-when 'freebsd'
-  fluentd_user_name    = 'fluentd'
-  fluentd_user_group   = 'fluentd'
-  fluentd_package_name = 'rubygem-fluentd'
-  fluentd_service_name = 'fluentd'
-  fluentd_config_path  = '/usr/local/etc/fluentd/fluent.conf'
-  fluentd_config_dir   = '/usr/local/etc/fluentd/conf.d'
-  fluentd_gem_bin       = '/usr/local/bin/fluent-gem'
-  fluentd_certs_dir    = '/usr/local/etc/fluentd/certs'
+when "freebsd"
+  fluentd_user_name    = "fluentd"
+  fluentd_user_group   = "fluentd"
+  fluentd_package_name = "rubygem-fluentd"
+  fluentd_service_name = "fluentd"
+  fluentd_config_dir   = "/usr/local/etc/fluentd/conf.d"
+  fluentd_gem_bin = "/usr/local/bin/fluent-gem"
+  fluentd_certs_dir = "/usr/local/etc/fluentd/certs"
 end
 
 describe package(fluentd_package_name) do
   it { should be_installed }
-end 
+end
 
 describe file(fluentd_config_dir) do
   it { should be_directory }
@@ -39,12 +37,12 @@ describe service(fluentd_service_name) do
 end
 
 describe command("#{fluentd_gem_bin} list") do
-  its(:stdout) { should_not match /^fluent-plugin-/ }
+  its(:stdout) { should_not match(/^fluent-plugin-/) }
 end
 
 describe file("#{fluentd_config_dir}/listen_on_5140.conf") do
-  its(:content) { should match /@type syslog/ }
-  its(:content) { should match /port 5140/ }
+  its(:content) { should match(/@type syslog/) }
+  its(:content) { should match(/port 5140/) }
 end
 
 describe port(5140) do
